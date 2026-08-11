0f7d6358fbbfd39eccd646a6ce51f5fd6dacc367
Preeti Parihar <160386193+preetiparihar82@users.noreply.github.com>
2026-08-11 14:12:20 +0530

    Update main.js

--- js/main.js
+++ js/main.js
@@ -175,7 +175,7 @@ document.addEventListener('DOMContentLoaded', function() {
             block2.classList.remove('active');
             block1.classList.add('active');
         }
-    }, 13000); 
+    }, 15000); 
 });
 
 
